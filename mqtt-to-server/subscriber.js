1576419b242a581fbc46074e636fe6ea6d6ba849
jinuklee <20181680@edu.hanbat.ac.kr>
2022-08-17 02:33:35 +0900

    아두이노 센서값을 받게 수정한 코드

--- mqtt-to-server/subscriber.js
+++ mqtt-to-server/subscriber.js
@@ -12,28 +12,21 @@ subclient.subscribe("smartgreen");
 //   "value":"26"}
 
 subclient.on('message', function (topic, message) {
-    let value = message.toString('utf-8');
-    let [sName,loc,tmp]=message.toString().split("/")
+    //let value = message.toString('utf-8');
+    let [sName,loc,tmp]=message.toString().split(",")
     let data= {
         "sensor_name":sName,
         "location":loc,
-        "value":value,
+        "value":tmp,
     };
     let args={
         sensor:{}
     };
     let sensors = JSON.stringify(data);
+
     console.log(sensors);
     args.sensor = sensors.toString('utf-8');
-
-    //console.log(message.toString('utf-8'));
-   console.log(data);
-    //console.log(value);
-// let data= {
-    //     "sensor_name":sName,
-    //     "location":loc,
-    //     "value":Number(tmp),
-    // };
+   console.log(args);
     // console.log(`key:${Object.keys(data)}, value:${Object.values(data)}`);
     // client.post("http://192.168.10.171:8080/api/sensors", args, function (data, response) {
     // js object로 파싱된 객체
